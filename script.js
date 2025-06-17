0e566e25e251208162d70b6b323173bb13900b5d
Virat <viratmpriya@yahoo.com>
2025-06-17 18:07:48 +0000

    why hello again

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Why hello there");
+alert("Why hello there...");
+alert("Looks like you found me.");
+var ask = confirm("Do you know why you are here?")
+if(ask == true){
+alert("Thats very good.");
+var key = prompt("Do you know the full access passkey?")
+}
+else{
+    window.close()
+}
