412fbc1cd68b25e5df6da5f75bd75b2057c83dd1
Virat <viratmpriya@yahoo.com>
2025-06-19 23:16:27 +0000

    yesjs

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Why hello there...");
+alert("Looks like you found me.");
+var ask = confirm("Do you know why you are here?");
+if(ask == true){
+alert("Thats very good.");
+var key = prompt("Do you know the full access passkey?");
+
+if(key == realkey){
+    prompt("Welcome! You have finally overcome...");
+    prompt("the stage of having to deal with me (javascript).");
+}
+
+}
+else{
+    window.close();
+}
