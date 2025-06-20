bed6bc5a375d607464a4bf0c38ac9d09986ccb19
Virat <viratmpriya@yahoo.com>
2025-06-20 00:22:50 +0000

    what

---
+++ riddle1.js
@@ -0,0 +1,2 @@
+alert("next step: learn something called one time pad");
+alert("https://www.youtube.com/watch?v=BGAY8vMyTAA");
--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 alert("Why hello there...");
 alert("Looks like you found me.");
+
 var ask = confirm("Do you know why you are here?");
-if(ask == true){
-alert("Thats very good.");
-var key = prompt("Do you know the full access passkey?");
 
-if(key == realkey){
-    prompt("Welcome! You have finally overcome...");
-    prompt("the stage of having to deal with me (javascript).");
-}
+if (ask === true) {
+    alert("That's very good.");
 
-}
-else{
+    var realkey = "realkey";
+    var success = false;
+
+    while (!success) {
+        var key = prompt("Do you know the full access passkey?");
+        
+        if (key === null) {
+            alert("You gave up... exiting.");
+            window.close();
+            break;
+        } else if (key === realkey) {
+            alert("Welcome! You have finally overcome the 1st stage");
+            success = true;
+        } else {
+            alert("Wrong key! Try again...");
+        }
+        break;
+        
+    }
+
+
+} else {
+    alert("Guess not... closing the window now.");
     window.close();
 }
