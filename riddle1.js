ce650c892fe64c36f12c8cc9708ccc5a0eb0f1e0
Virat <viratmpriya@yahoo.com>
2025-06-20 00:44:28 +0000

    IM DONE, WITH PART 1!!!!!!!!!!

--- riddle1.js
+++ riddle1.js
@@ -1,2 +1,11 @@
 alert("next step: learn something called one time pad");
 alert("https://www.youtube.com/watch?v=BGAY8vMyTAA");
+var a = confirm("You done?");
+if(a == true){
+    alert("great, you can move on to the second test.")
+    alert("what is the real word if...");
+var  riddle = prompt("you are given the code 54FDA ? NO CAPS");
+}
+if(riddle == great){
+alert("CONGRADULATIONS, you passed part one, of the game of bordom. Feel free to boast as much as you like to your freinds.");
+}
